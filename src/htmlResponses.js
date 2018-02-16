4fe2909f9039e092303e06ced5158fed82d04581
Mitch <Crimson-Catalyst@users.noreply.github.com>
2018-02-16 15:29:36 -0500

    added rudiments of code

---
+++ src/htmlResponses.js
@@ -0,0 +1,22 @@
+const fs = require('fs'); // pull in file module
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const styles = fs.readFileSync(`${__dirname}/../client/style.css`);
+
+//return index file
+const getIndex = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(index);
+  response.end();
+};
+
+//return css
+const getStyles = (request, response) => {
+  response.writeHead(200, { 'Content-Type': 'text/css' });
+  response.write(styles);
+  response.end();
+};
+
+//exports
+module.exports.getIndex = getIndex;
+module.exports.getStyles = getStyles;
---
+++ src/jsonResponses.js
@@ -0,0 +1,23 @@
+// json response function
+const respondJSON = (request, response, status, object) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+//exports
+module.exports = {
+  
+};
